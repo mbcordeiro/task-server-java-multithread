be69a028b32d9b3cd64795c10ccff5a45d3dfabe
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-05-13 00:28:55 -0300

    Thread clients

--- src/br/com/server/client/TaskClient.java
+++ src/br/com/server/client/TaskClient.java
@@ -6,20 +6,48 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class TaskClient {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Socket socket = new Socket("localhost", 8882);
 
         System.out.println("Connection on...");
+        Thread threadSendCommandServer = new Thread(() -> {
+            try {
+                PrintStream printStream = new PrintStream(socket.getOutputStream());
+                Scanner scanner = new Scanner(System.in);
+                while (scanner.hasNextLine()) {
+                    String line = scanner.nextLine();
+                    if (line.trim().equals("")) {
+                        break;
+                    }
+                    printStream.println(line);
+                }
+                printStream.close();
+                scanner.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-        PrintStream printStream = new PrintStream(socket.getOutputStream());
-        printStream.println("Commnad 1");
+        Thread threadResponseServer = new Thread(() -> {
+            System.out.println("Getting data from the server...");
+            try {
+                Scanner inputStream = new Scanner(socket.getInputStream());
+                while (inputStream.hasNextLine()) {
+                    String line = inputStream.nextLine();
+                    System.out.println(line);
+                }
+                inputStream.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
-        Scanner scanner = new Scanner(System.in);
+        threadSendCommandServer.start();
+        threadResponseServer.start();
 
-        scanner.nextLine();
+        threadSendCommandServer.join();
 
+        System.out.println("Closing client socket...");
         socket.close();
-        printStream.close();
-        scanner.close();
     }
 }
