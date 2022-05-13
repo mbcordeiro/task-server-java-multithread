6338dad176ca417f7fe6b0fe1a3044dde49e52e2
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2022-05-12 23:47:00 -0300

    Thread pool config

--- src/br/com/server/TaskServer.java
+++ src/br/com/server/TaskServer.java
@@ -5,6 +5,8 @@ import br.com.server.thread.DistributeTasks;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class TaskServer {
     public static void main(String[] args) throws IOException {
@@ -13,13 +15,14 @@ public class TaskServer {
 
         ServerSocket server = new ServerSocket(port);
 
+        ExecutorService threadPool = Executors.newCachedThreadPool();
+
         while (true) {
             Socket socket = server.accept();
             System.out.println("Accepting new client in port" + socket.getPort());
 
             DistributeTasks distributeTasks = new DistributeTasks(socket);
-            Thread thread = new Thread(distributeTasks);
-            thread.start();
+            threadPool.execute(distributeTasks);
         }
     }
 }
